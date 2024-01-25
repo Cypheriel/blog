f078bf6f5db38481a55c4ab679b8fe9084a540df
Cypheriel <61402923+Cypheriel@users.noreply.github.com>
2024-01-25 13:26:26 -0700

    spice up blog post page

--- src/routes/post/[slug]/+page.svelte
+++ src/routes/post/[slug]/+page.svelte
@@ -3,9 +3,12 @@
 
     /** @type {import('./$types').PageData} */
     export let data;
-
-    const content = data.post.Content
 </script>
 
-<h1>{data.post.Title}</h1>
-<SvelteMarkdown source={data.post.Content} />
+<a class="absolute btn z-10 m-5 bg-secondary/70 text-white" href="../">Back</a>
+<div class="w-screen lg:w-4/5 2xl:w-3/5 mx-auto z-0">
+    <article class="z-0 prose prose-lg max-w-full bg-base-300 px-10 pt-20 pb-16 justify-center min-h-screen">
+        <h1 class="text-center">{data.post.Title}</h1>
+        <SvelteMarkdown source={data.post.Content} />
+    </article>
+</div>
