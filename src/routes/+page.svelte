5ca8e14ea74f62aa58c7e2ecbd87144ee93265c8
Cypheriel <61402923+Cypheriel@users.noreply.github.com>
2024-01-24 23:23:30 -0700

    add TailwindCSS and daisyUI

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,12 +3,34 @@
     export let data;
 </script>
 
-<h1>Cypheriel's Thrilling Tales</h1>
-<h3>Posts:</h3>
-<ul>
-{#each data.posts as post}
-    <li>
-        <a href="./post/{post.Slug}">{post.Title}</a>
-    </li>
-{/each}
-</ul>
+<svelte:head>
+</svelte:head>
+
+
+<div class="w-screen text-center py-5 px-3">
+    <h1 class="text-5xl font-bold">Cypheriel's Thrilling Tales</h1>
+    <p class="py-4">This is a blog.</p>
+</div>
+
+<div class="flex flex-row min-w-fit mx-5 gap-3">
+    <input type="text" placeholder="This is not yet functional..." class="input input-bordered input-secondary flex-grow" />
+    <div class="tooltip" data-tip="wtf are you doing">
+        <button type="submit" class="btn">Search(?)</button>
+    </div>
+</div>
+
+<div class="px-4 py-4">
+    <div class="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-5">
+        {#each data.posts as post}
+            <a class="card card-compact bg-base-300 shadow-xl w-80 hover:outline" href="./post/{post.Slug}">
+                <div class="card-body">
+                    <h2 class="card-title">
+                        {post.Title}
+                        <div class="badge">01/24/2024</div>
+                    </h2>
+                    <p>TODO: Add short descriptions/subtitles.</p>
+                </div>
+            </a>
+        {/each}
+    </div>
+</div>
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+    content: ["./src/**/*.{html,js,ts,svelte}"],
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require("daisyui"),
+        require('@tailwindcss/typography'),
+    ],
+}
+
