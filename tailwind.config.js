682a17e553ce9b36a6639c839c5fdbebc4e31bdb
Cypheriel <61402923+Cypheriel@users.noreply.github.com>
2024-01-31 20:11:05 -0700

    customize theme colors and change favicon

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,28 @@ export default {
     },
     plugins: [
         require("daisyui"),
-        require('@tailwindcss/typography'),
+        require("@tailwindcss/typography"),
     ],
+    daisyui: {
+        themes: [
+            {
+                dark: {
+                    ...require("daisyui/src/theming/themes")["dark"],
+                    primary: "#bb99ff",
+                    secondary: "#99ddff",
+                    accent: "#ddccff",
+                    warning: "#ffee99"
+                }
+            },
+            {
+                light: {
+                    ...require("daisyui/src/theming/themes")["light"],
+                    primary: "#bb99ff",
+                    secondary: "#99ddff",
+                    accent: "#ddccff",
+                    warning: "#ffee99"
+                }
+            }
+        ],
+    },
 }
-
