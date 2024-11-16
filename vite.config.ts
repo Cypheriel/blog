03b396bb79689392f73830f3b1de1db0cccd14ea
Mikel <61402923+Cypheriel@users.noreply.github.com>
2024-11-16 04:57:16 -0700

    remove node polyfills

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,6 @@
 import {sveltekit} from '@sveltejs/kit/vite';
 import {defineConfig} from 'vite';
-import {nodePolyfills} from "vite-plugin-node-polyfills";
 
 export default defineConfig({
-    plugins: [sveltekit(), nodePolyfills()]
+    plugins: [sveltekit()]
 });
